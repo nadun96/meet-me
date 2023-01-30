aacf6a662f1aface9bc4451240c9aef12c08ed2b
ladchumananaaruran <ladchumananaaruran@gmail.com>
2023-01-30 22:33:07 +0530

    google auth

--- StackNavigator.js
+++ StackNavigator.js
@@ -3,18 +3,28 @@ import React from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import HomeScreen from './screens/HomeScreen';
 import Login from './screens/Login';
+import useAuth from './hooks/useAuth';
 
 const Stack = createNativeStackNavigator();
 
 const StackNavigator = () => {
+    const {user} = useAuth();
     return (
         <Stack.Navigator
             screenOptions={{
                 headerShown: false,
             }}
         >
-            <Stack.Screen name="Home" component={HomeScreen} />
-            <Stack.Screen name="Login" component={Login} />
+            { user ? (
+                <>
+                    <Stack.Screen name="Home" component={HomeScreen} />
+                </>
+            ) : (
+                    <Stack.Screen name="Login" component={Login} />
+            )}
+        
+            
+            
         </Stack.Navigator>
     )
 }
---
+++ hooks/useAuth.js
@@ -0,0 +1,80 @@
+import React, { createContext, useContext, useState, useEffect, useMemo } from "react";
+import * as Google from "expo-google-app-auth";
+import { GoogleAuthProvider, onAuthStateChanged, signInWithCredential, signOut,} from "@firebase/auth";
+import { auth } from "../firebase";
+  
+const AuthContext = createContext({});
+  
+const config= {
+    iosClintId: '672293511257-jb4tbepkrkahk42qkitk0bmnci5ldd9k.apps.googleusercontent.com',
+    androidClintId: '672293511257-qvimg9n1i1fjfjn18sndg97aalvp4d8n.apps.googleusercontent.com',
+    scopes: ["profile", "email"],
+    permissions: ["public_profile", "email", "gender", "location"]
+}
+  
+export const AuthProvider = ({ children }) => {
+    const [error, setError] = useState(null);
+    const [user, setUser] = useState(null);
+    const [loadingInitial, setLoadingInitial] = useState(true);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(
+        () =>
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                setUser(user);
+            } else {
+                setUser(null);
+            }
+
+            setLoadingInitial(false);
+        }),
+        []
+    );
+
+    const logout = () => {
+        setLoading(true);
+
+        signOut(auth)
+        .catch((error) => setError(error))
+        .finally(() => setLoading(false));
+    };
+
+    const signInWithGoogle = async () => {
+        setLoading(true);
+
+        await Google.logInAsync(config).then(async (logInResult) => {
+            if (logInResult.type === "success") {
+                const { idToken, accessToken } = logInResult;
+                const credential = GoogleAuthProvider.credential(idToken, accessToken);
+
+                await signInWithCredential(auth, credential);
+            }
+            return Promise.reject();
+        })
+        .catch((error) => setError(error))
+        .finally(() => setLoading(false));
+    };
+
+    const memoedValue = useMemo(
+        () => ({
+        user,
+        loading,
+        error,
+        signInWithGoogle,
+        logout,
+        }),
+        [user, loading, error]
+    );
+
+    return (
+        <AuthContext.Provider value={memoedValue}>
+        {!loadingInitial && children}
+        </AuthContext.Provider>
+    );
+};
+  
+export default function useAuth() {
+    return useContext(AuthContext);
+}
+  
