ae9f3fd7d529a57f49946bc2423a5f378a41ee79
Shamika Tissera <nimeshshamika@gmail.com>
2023-02-01 13:07:08 +0530

    app re-init

--- App.js
+++ App.js
@@ -1,17 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { LogBox } from 'react-native';
-LogBox.ignoreAllLogs();
-import StackNavigator from './StackNavigator';
-import { NavigationContainer } from '@react-navigation/native';
-//import { AuthProvider } from './hooks/useAuth';
-import tw from 'tailwind-rn';
 
 export default function App() {
   return (
-    <View style={tw("justify-center items-center")}>
-      <Text>App</Text>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
     </View>
   );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,4 @@
 module.exports = function(api) {
-  plugins: ["nativewind/babel"],
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
