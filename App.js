660695d9a147d00b2511d06b1fda9dd959c093ba
Shamika Tissera <nimeshshamika@gmail.com>
2023-02-02 11:01:58 +0530

    Updated files to match new requirements

--- App.js
+++ App.js
@@ -1,21 +1,35 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import {TailwindProvider} from 'tailwind-rn';
-import utilities from './tailwind.json';
-import { useTailwind } from 'tailwind-rn';
-import StackNavigator from './StackNavigator';
-import { NavigationContainer } from '@react-navigation/native';
-import { AuthProvider } from './hooks/useAuth';
+import { NavigationContainer, StackActions } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
+
+const Stack = createNativeStackNavigator();
+
+const globalScreenOptions = {
+  headerStyle: { backgroundColor: "#2C6BED" },
+  headerTitleStyle: { color: "white" },
+  headerTintColor: "white",
+}
 
 export default function App() {
   return (
-    <TailwindProvider utilities={utilities}>
-      <NavigationContainer>
-        <AuthProvider>
-          <StackNavigator />
-        </AuthProvider>
-      </NavigationContainer>
-    </TailwindProvider>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={globalScreenOptions}>
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Register" component={RegisterScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,14 +1,73 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import useAuth from '../hooks/useAuth';
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { Button, Input, Image } from "react-native-elements";
+import { StatusBar } from "expo-status-bar";
+import { KeyboardAvoidingView } from "react-native";
 
-const LoginScreen = () => {
-    const { user } = useAuth();
-  return (
-    <View>
-      <Text>LoginScreen</Text>
-    </View>
-  )
-}
+const LoginScreen = ({ navigation }) => {
+    const [email, setEmail] = React.useState("");
+    const [password, setPassword] = React.useState("");
 
-export default LoginScreen
+    const signIn = () => { };
+
+    return (
+        <KeyboardAvoidingView behavior="padding" style={styles.container}>
+            <StatusBar style="light" />
+            <Image
+                source={{
+                    uri: "https://www.shutterstock.com/image-vector/wifi-icon-interface-design-vector-260nw-1448076602.jpg",
+                }}
+                style={{ width: 200, height: 200 }}
+            />
+            <View style={styles.inputContainer}>
+                <Input
+                    placeholder="Email"
+                    autofocus
+                    type="email"
+                    autoFocus
+                    value={email}
+                    onChangeText={(text) => setEmail(text)}
+                    containerStyle={styles.button}
+                />
+                <Input
+                    placeholder="Password"
+                    secureTextEntry
+                    type="password"
+                    value={password}
+                    onChangeText={(text) => setPassword(text)}
+                    containerStyle={styles.button}
+                />
+            </View>
+            <Button
+                containerStyle={styles.button}
+                onPress={signIn}
+                title="Login"
+            />
+            <Button
+            onPress={() => navigation.navigate("Register")}
+                containerStyle={styles.button}
+                type="outline"
+                title="Register"
+            />
+        </KeyboardAvoidingView>
+    );
+};
+
+export default LoginScreen;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        padding: 10,
+        backgroundColor: "white",
+    },
+    inputContainer: {
+        width: 300,
+    },
+    button: {
+        width: 200,
+        marginTop: 10,
+    },
+});
